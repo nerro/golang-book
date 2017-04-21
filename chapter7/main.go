cad423d556a81bd4fd42aabd91c6c068eae2027a
Pavel Sorokin <pavel.sorokin@nerro.eu>
2017-04-21 11:42:02 +0200

    Add functions (chapter7)

---
+++ chapter7/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func average(xs []float64) float64 {
+	total := 0.0
+	for _, v := range xs {
+		total += v
+	}
+	return total / float64(len(xs))
+}
+
+func add(args ...int) int {
+	total := 0
+	for _, v := range args {
+		total += v
+	}
+	return total
+}
+
+func main() {
+	xs := []float64{98, 93, 77, 82, 83}
+	fmt.Println(average(xs))
+
+	xy := []int{1, 2, 3}
+	fmt.Println(add(xy...))
+}
