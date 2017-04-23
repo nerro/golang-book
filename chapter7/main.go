92741d3877c00c98a1d13e78ccfc7b160413f160
Pavel Sorokin <pavel.sorokin@nerro.eu>
2017-04-23 18:59:04 +0200

    Add closure examples (chapter7)

--- chapter7/main.go
+++ chapter7/main.go
@@ -18,10 +18,38 @@ func add(args ...int) int {
 	return total
 }
 
+func makeEvenGenerator() func() uint {
+	i := uint(0)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
+	}
+}
+
 func main() {
 	xs := []float64{98, 93, 77, 82, 83}
 	fmt.Println(average(xs))
 
 	xy := []int{1, 2, 3}
 	fmt.Println(add(xy...))
+
+	//closure
+	add := func(x, y int) int {
+		return x + y
+	}
+	fmt.Println(add(1, 1))
+
+	x := 0
+	increment := func() int {
+		x++
+		return x
+	}
+	fmt.Println(increment())
+	fmt.Println(increment())
+
+	nextEven := makeEvenGenerator()
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
 }
